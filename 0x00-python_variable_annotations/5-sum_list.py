685bb8d083b245524a21ea9923361cbcace44327
rashaaz <rashaabdelaziz111@gmail.com>
2024-06-05 21:22:49 -0700

    add t5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" This module contains a function """
+from typing import Callable, Iterator, Union, Optional, List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Sums a list of floats
+    returns The sum of the float numbers in the list
+    """
+
+    return sum(input_list)
