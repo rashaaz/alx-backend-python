5178911727c3267ee0f921c70661710d5a8973e2
rashaaz <rashaabdelaziz111@gmail.com>
2024-06-05 21:07:09 -0700

    add t0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+'''
+    This module contains a function
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+        Adds two float numbers
+    '''
+    return a + b
